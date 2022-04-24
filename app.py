6283a08b21fc26076aaac4120729d25f9da3a888
KungFuKennyOG <79944172+KungFuKennyOG@users.noreply.github.com>
2022-04-24 17:21:28 -0400

    modified aap.py

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 
 learn = load_learner('trained_model.pkl')
 
+
 def predict_image(img):
     prediction = learn.predict(PILImage(img))
     if prediction[0] == 'hot_dog':
         return 'hot dog'
     return 'not hot dog'
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     return predict_image(request.files['image'])
 
-if __name__ = '__main__' :
+
+if __name__ == '__main__':
     app.run
-
-
