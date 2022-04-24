e771dd2eeb4e4cf51afc01ee4bb19011be3a13aa
KungFuKennyOG <79944172+KungFuKennyOG@users.noreply.github.com>
2022-04-24 17:19:56 -0400

    added a bunch of stuff lol

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from fastai.basics import *
+from fastai.vision.all import *
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+
+learn = load_learner('trained_model.pkl')
+
+def predict_image(img):
+    prediction = learn.predict(PILImage(img))
+    if prediction[0] == 'hot_dog':
+        return 'hot dog'
+    return 'not hot dog'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    return predict_image(request.files['image'])
+
+if __name__ = '__main__' :
+    app.run
+
+
